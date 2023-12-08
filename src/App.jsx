207229d28a309e714fdfe106e09703ae9a78777e
PRATHAMPARLECHA <prathamparlecha09@gmail.com>
2023-12-08 23:50:47 +0530

    TimerGame

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Player from './components/Player.jsx';
+import Timer from './components/Timer.jsx';
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div id="challenges">
+        <Timer title="Easy" targetTime={1}/>
+        <Timer title="Not easy" targetTime={5}/>
+        <Timer title="Gettin tough" targetTime={10} />
+        <Timer title="Pros only" targetTime={15}/>
+      </div>
+    </>
+  );
+}
+
+export default App;
